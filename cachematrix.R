6618798c55f77d5b123dfe7d090212fb6be7fba9
Andy Beaton <aiabx@interlog.com>
2015-03-22 15:03:39 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,32 @@
 ## functions do
 
 ## Write a short comment describing this function
+## I have no idea what I'm doing as far as these functions are concerned
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }  
+}
 
+
+cachesolve <- function(x, ...) {
+  m <- x$solve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$solve()
+  m <- solve(data, ...)
+  x$solve(m)
+  m
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
 }
